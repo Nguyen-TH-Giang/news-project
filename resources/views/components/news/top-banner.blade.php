68174c955d1aba053859657b752de80b65da935e
Nguyen-TH-Giang <124768986+Nguyen-TH-Giang@users.noreply.github.com>
2023-08-02 11:40:54 +0700

    News: extracted wrong, fix top-banner component

--- resources/views/components/news/top-banner.blade.php
+++ resources/views/components/news/top-banner.blade.php
@@ -1,5 +1,3 @@
-<div class="col-lg-4">
-    <a href="/" class="navbar-brand d-none d-lg-block">
-        <h1 class="m-0 display-5 text-uppercase"><span class="text-primary">News</span>Room</h1>
-    </a>
+<div class="col-lg-8 text-center text-lg-right">
+    <img class="img-fluid" src="news/img/ads-700x70.jpg" alt="">
 </div>
