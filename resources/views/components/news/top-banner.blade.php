8d1b74e0b932125cb36ef68250f69c8f33350e40
Nguyen-TH-Giang <124768986+Nguyen-TH-Giang@users.noreply.github.com>
2023-08-02 11:39:34 +0700

    News: extract top-banner component

---
+++ resources/views/components/news/top-banner.blade.php
@@ -0,0 +1,5 @@
+<div class="col-lg-4">
+    <a href="/" class="navbar-brand d-none d-lg-block">
+        <h1 class="m-0 display-5 text-uppercase"><span class="text-primary">News</span>Room</h1>
+    </a>
+</div>
