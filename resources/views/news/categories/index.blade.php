25ce07fe676a66b501f9b249712ccd626c631c89
Nguyen-TH-Giang <124768986+Nguyen-TH-Giang@users.noreply.github.com>
2023-08-02 17:02:12 +0700

    News: add categories page template

---
+++ resources/views/news/categories/index.blade.php
@@ -0,0 +1,97 @@
+<x-news.layout>
+    <!-- Breadcrumb Start -->
+    <div class="container-fluid">
+        <div class="container">
+            <nav class="breadcrumb bg-transparent m-0 p-0">
+                <a class="breadcrumb-item" href="#">Home</a>
+                <a class="breadcrumb-item" href="#">Category</a>
+                <span class="breadcrumb-item active">Technology</span>
+            </nav>
+        </div>
+    </div>
+    <!-- Breadcrumb End -->
+
+    <x-news.news-with-sidebar>
+        <div class="col-lg-8">
+            <div class="row">
+                <div class="col-12">
+                    <div class="d-flex align-items-center justify-content-between bg-light py-2 px-4 mb-3">
+                        <h3 class="m-0">Technology</h3>
+                        <a class="text-secondary font-weight-medium text-decoration-none" href="">View
+                            All</a>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <x-news.big-news-item />
+                </div>
+                <div class="col-lg-6">
+                    <x-news.big-news-item />
+                </div>
+                <div class="col-lg-6">
+                    <x-news.big-news-item />
+                </div>
+                <div class="col-lg-6">
+                    <x-news.big-news-item />
+                </div>
+            </div>
+
+            <x-news.center-banner />
+
+            <div class="row">
+                <div class="col-lg-6">
+                    <x-news.small-news-item />
+                </div>
+                <div class="col-lg-6">
+                    <x-news.small-news-item />
+                </div>
+                <div class="col-lg-6">
+                    <x-news.small-news-item />
+                </div>
+                <div class="col-lg-6">
+                    <x-news.small-news-item />
+                </div>
+                <div class="col-lg-6">
+                    <x-news.small-news-item />
+                </div>
+                <div class="col-lg-6">
+                    <x-news.small-news-item />
+                </div>
+                <div class="col-lg-6">
+                    <x-news.small-news-item />
+                </div>
+                <div class="col-lg-6">
+                    <x-news.small-news-item />
+                </div>
+                <div class="col-lg-6">
+                    <x-news.small-news-item />
+                </div>
+                <div class="col-lg-6">
+                    <x-news.small-news-item />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <nav aria-label="Page navigation">
+                        <ul class="pagination justify-content-center">
+                            <li class="page-item disabled">
+                                <a class="page-link" href="#" aria-label="Previous">
+                                    <span class="fa fa-angle-double-left" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                            </li>
+                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item">
+                                <a class="page-link" href="#" aria-label="Next">
+                                    <span class="fa fa-angle-double-right" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </x-news.news-with-sidebar>
+</x-news.layout>
