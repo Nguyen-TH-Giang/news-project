d1cdb70830137303028729babc96fb000779af2b
Nguyen-TH-Giang <124768986+Nguyen-TH-Giang@users.noreply.github.com>
2023-08-02 09:59:49 +0700

    News: build main page template

---
+++ resources/views/news/posts/index.blade.php
@@ -0,0 +1,7 @@
+<x-news.layout>
+    <x-news.top-news-slider />
+    <x-news.main-news-slider />
+    <x-news.featured-news-slider />
+    <x-news.category-news-slider />
+    <x-news.news-with-sidebar />
+</x-news.layout>
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('news.posts.index');
 });
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
