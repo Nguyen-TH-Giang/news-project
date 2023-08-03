975fc7782af6d95a8af04bbb26ec6a6ecce2af73
Nguyen-TH-Giang <124768986+Nguyen-TH-Giang@users.noreply.github.com>
2023-08-03 15:56:22 +0700

    Admin: extract breadcrumb component

---
+++ resources/views/components/admin/breadcrumb.blade.php
@@ -0,0 +1,11 @@
+@props(['items'])
+
+<nav>
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="/">Home</a></li>
+
+        @foreach ($items as $key => $item)
+            <li class="breadcrumb-item {{ $loop->last ? ' active' : '' }}">{{ $item['label'] }}</li>
+        @endforeach
+    </ol>
+</nav>
