aed38035344fc8d3b5fd7e9037a003c8dc327294
Nguyen-TH-Giang <124768986+Nguyen-TH-Giang@users.noreply.github.com>
2023-08-03 14:03:31 +0700

    Admin: extract form field component, update on input and textarea component

--- resources/views/components/admin/form/input.blade.php
+++ resources/views/components/admin/form/input.blade.php
@@ -1,9 +1,10 @@
 @props(['name', 'type' => 'text', 'label'])
 
-<div class="row mb-3">
+<x-admin.form.field>
     <label for="{{ $name }}" class="col-sm-2 col-form-label">{{ ucwords($label) }}</label>
     <div class="col-sm-10">
         <input class="form-control" type="{{ $type }}" name="{{ $name }}" id="{{ $name }}" {{ $attributes(['value' => old($name)]) }}>
     </div>
     <x-admin.form.error name="{{ $name }}" />
-</div>
+</x-admin.form.field>
+
--- resources/views/components/admin/form/textarea.blade.php
+++ resources/views/components/admin/form/textarea.blade.php
@@ -1,9 +1,9 @@
 @props(['name', 'id', 'label'])
 
-<div class="row mb-3">
+<x-admin.form.field>
     <label for="{{ $name }}" class="col-sm-2 col-form-label">{{ ucwords($label) }}</label>
     <div class="col-sm-10">
         <textarea class="form-control" style="height: 500px" id="{{ $id }}" name="{{ $name }}"></textarea>
     </div>
     <x-admin.form.error name="{{ $name }}" />
-</div>
+</x-admin.form.field>
