be89f58b6529d0903e5f20445ea1d9427d094685
Nguyen-TH-Giang <124768986+Nguyen-TH-Giang@users.noreply.github.com>
2023-08-04 09:52:53 +0700

    Admin: modified forgot password page template

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,36 +1,51 @@
 <x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+    <div class="container">
+        <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+                        <x-auth-card>
+                            <x-slot name="logo">
+                                <div class="d-flex justify-content-center py-4">
+                                    <a href="/" class="logo d-flex align-items-center w-auto">
+                                        <x-application-logo />
+                                    </a>
+                                </div><!-- End Logo -->
+                            </x-slot>
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-        </div>
+                            <div>
+                                {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+                            </div>
+                            <!-- Session Status -->
+                            <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+                            <!-- Validation Errors -->
+                            <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                            <form class="row g-3 needs-validation" method="POST" action="{{ route('password.email') }}" novalidate>
+                                @csrf
+                                <div class="col-12">
+                                    <x-label for="email" :value="__('Email')" />
 
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
+                                    <div class="input-group has-validation">
+                                        <span class="input-group-text" id="inputGroupPrepend">@</span>
+                                        <input type="text" name="email" class="form-control"
+                                            id="email" value="{{ old('email') }}" required>
+                                        <div class="invalid-feedback">Please enter your username.</div>
+                                    </div>
+                                </div>
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+                                <div class="col-12">
+                                    <button class="btn btn-primary w-100" type="submit">{{ __('Email Password Reset Link') }}</button>
+                                </div>
+                                </div>
+                            </form>
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                        </x-auth-card>
+                    </div>
+                </div>
             </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
+        </section>
+    </div>
 </x-guest-layout>
+
