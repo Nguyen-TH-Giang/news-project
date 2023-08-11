c2d7fa8b72c932f9468a3f4d209fa329edf2a482
Nguyen-TH-Giang <124768986+Nguyen-TH-Giang@users.noreply.github.com>
2023-08-11 17:50:49 +0700

    Change the template, adding search to banner

--- resources/views/admin/banners/create.blade.php
+++ resources/views/admin/banners/create.blade.php
@@ -7,58 +7,56 @@
 
         <section class="section dashboard">
             <div class="row">
-
-                <!-- Left side columns -->
                 <div class="col-lg-12">
-                    <div class="row">
-
-                        <!-- General Form Elements -->
-                        <form method="POST" action="/admin/banners" enctype="multipart/form-data">
-                            @csrf
-                            <x-admin.form.input name="title" type="text" label="title" :value="old('title')">
-                                <x-admin.required-icon />
-                            </x-admin.form.input>
-
-                            <x-admin.form.input name="image_url" type="file" label="image">
-                                <x-admin.required-icon />
-                            </x-admin.form.input>
-
-                            <fieldset class="row mb-3">
-                                <legend class="col-form-label col-sm-2 pt-0">Type
+                    <div class="card">
+                        <div class="card-body">
+                            <form method="POST" action="/admin/banners" enctype="multipart/form-data" class="mt-4">
+                                @csrf
+                                <x-admin.form.input name="title" type="text" label="title" :value="old('title')">
                                     <x-admin.required-icon />
-                                </legend>
-                                <div class="col-sm-10">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="type" id="gridRadios1" value="{{ Constants::BANNER_TOP }}" {{ old('type') == Constants::BANNER_TOP ? 'checked' : '' }}>
-                                        <label class="form-check-label" for="gridRadios1">Top banner 700x70</label>
+                                </x-admin.form.input>
+
+                                <x-admin.form.input name="image_url" type="file" label="image">
+                                    <x-admin.required-icon />
+                                </x-admin.form.input>
+
+                                <fieldset class="row mb-3">
+                                    <legend class="col-form-label col-sm-2 pt-0">Type
+                                        <x-admin.required-icon />
+                                    </legend>
+                                    <div class="col-sm-10">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="type" id="gridRadios1" value="{{ Constants::BANNER_TOP }}" {{ old('type') == Constants::BANNER_TOP ? 'checked' : '' }}>
+                                            <label class="form-check-label" for="gridRadios1">Top banner 700x70</label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="type" id="gridRadios2" value="{{ Constants::BANNER_SIDE }}" {{ old('type') == Constants::BANNER_SIDE ? 'checked' : '' }}>
+                                            <label class="form-check-label" for="gridRadios2">Side banner 500x280</label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="type" id="gridRadios2" value="{{ Constants::BANNER_CENTER }}" {{ old('type') == Constants::BANNER_CENTER ? 'checked' : '' }}>
+                                            <label class="form-check-label" for="gridRadios2">Center banner 700x70</label>
+                                        </div>
                                     </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="type" id="gridRadios2" value="{{ Constants::BANNER_SIDE }}" {{ old('type') == Constants::BANNER_SIDE ? 'checked' : '' }}>
-                                        <label class="form-check-label" for="gridRadios2">Side banner 500x280</label>
-                                    </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="type" id="gridRadios2" value="{{ Constants::BANNER_CENTER }}" {{ old('type') == Constants::BANNER_CENTER ? 'checked' : '' }}>
-                                        <label class="form-check-label" for="gridRadios2">Center banner 700x70</label>
-                                    </div>
-                                </div>
-                                <x-admin.form.error name="type" />
-                            </fieldset>
+                                    <x-admin.form.error name="type" />
+                                </fieldset>
 
-                            <x-admin.form.input name="date" type="date" label="Published date" :value="old('date')">
-                                <x-admin.required-icon />
-                            </x-admin.form.input>
+                                <x-admin.form.input name="date" type="date" label="Published date" :value="old('date')">
+                                    <x-admin.required-icon />
+                                </x-admin.form.input>
 
-                            <x-admin.form.input name="time" type="time" label="Time" :step="1" :value="old('time')">
-                                <x-admin.required-icon />
-                            </x-admin.form.input>
+                                <x-admin.form.input name="time" type="time" label="Time" :step="1" :value="old('time')">
+                                    <x-admin.required-icon />
+                                </x-admin.form.input>
 
-                            <x-admin.form.checkbox name="status" legend="active" :checked="old('status') == Constants::ACTIVE"/>
+                                <x-admin.form.checkbox name="status" legend="active" :checked="old('status') == Constants::ACTIVE"/>
 
-                            <x-admin.form.button route="{{ route('admin.banners.index') }}">Create</x-admin.form.button>
+                                <x-admin.form.button route="{{ route('admin.banners.index') }}">Create</x-admin.form.button>
 
-                        </form><!-- End General Form Elements -->
+                            </form>
+                        </div>
                     </div>
-                </div><!-- End Left side columns -->
+                </div>
             </div>
         </section>
     </main><!-- End #main -->
--- resources/views/admin/banners/edit.blade.php
+++ resources/views/admin/banners/edit.blade.php
@@ -10,67 +10,66 @@
 
         <section class="section dashboard">
             <div class="row">
-
-                <!-- Left side columns -->
                 <div class="col-lg-12">
-                    <div class="row">
-                        <!-- General Form Elements -->
-                        <form method="POST" action="/admin/banners/{{ $banner->id }}">
-                            @csrf
-                            @method('PATCH')
+                    <div class="card">
+                        <div class="card-body">
+                            <form method="POST" action="/admin/banners/{{ $banner->id }}" class="mt-4">
+                                @csrf
+                                @method('PATCH')
 
-                            <x-admin.form.input name="title" type="text" label="title" :value="old('title', $banner->title)">
-                                <x-admin.required-icon />
-                            </x-admin.form.input>
-
-                            <div class="d-flex flex-column">
-                                <div>
-                                    <x-admin.form.input name="thumbnail" type="file" label="thumbnail" />
-                                </div>
-                                <div class="align-self-center">
-                                    @php
-                                        $path = public_path('/storage/' . $banner->image_url);
-                                        $imageSrc = File::exists($path) && !is_dir($path) ? asset('storage/' . $banner->image_url) : Constants::BANNER_PLACEHOLDER;
-                                    @endphp
-                                    <img src="{{ $imageSrc }}" alt="{{ $banner->image_url }}" width="100">
-                                </div>
-                            </div>
-
-                            <fieldset class="row mb-3">
-                                <legend class="col-form-label col-sm-2 pt-0">Type
+                                <x-admin.form.input name="title" type="text" label="title" :value="old('title', $banner->title)">
                                     <x-admin.required-icon />
-                                </legend>
-                                <div class="col-sm-10">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="type" id="gridRadios1" value="{{ Constants::BANNER_TOP }}" @if(old('type', $banner->type) == Constants::BANNER_TOP) checked @endif>
-                                        <label class="form-check-label" for="gridRadios1">Top banner 700x70</label>
+                                </x-admin.form.input>
+
+                                <div class="d-flex flex-column">
+                                    <div>
+                                        <x-admin.form.input name="thumbnail" type="file" label="thumbnail" />
                                     </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="type" id="gridRadios2" value="{{ Constants::BANNER_SIDE }}" @if(old('type', $banner->type) == Constants::BANNER_SIDE) checked @endif>
-                                        <label class="form-check-label" for="gridRadios2">Side banner 500x280</label>
-                                    </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="type" id="gridRadios2" value="{{ Constants::BANNER_CENTER }}" @if(old('type', $banner->type) == Constants::BANNER_CENTER) checked @endif>
-                                        <label class="form-check-label" for="gridRadios2">Center banner 700x70</label>
+                                    <div class="align-self-center">
+                                        @php
+                                            $path = public_path('/storage/' . $banner->image_url);
+                                            $imageSrc = File::exists($path) && !is_dir($path) ? asset('storage/' . $banner->image_url) : Constants::BANNER_PLACEHOLDER;
+                                        @endphp
+                                        <img src="{{ $imageSrc }}" alt="{{ $banner->image_url }}" width="100">
                                     </div>
                                 </div>
-                            </fieldset>
 
-                            <x-admin.form.input name="date" type="date" label="Published date" :value="old('date', $banner->date)">
-                                <x-admin.required-icon />
-                            </x-admin.form.input>
+                                <fieldset class="row mb-3">
+                                    <legend class="col-form-label col-sm-2 pt-0">Type
+                                        <x-admin.required-icon />
+                                    </legend>
+                                    <div class="col-sm-10">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="type" id="gridRadios1" value="{{ Constants::BANNER_TOP }}" @if(old('type', $banner->type) == Constants::BANNER_TOP) checked @endif>
+                                            <label class="form-check-label" for="gridRadios1">Top banner 700x70</label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="type" id="gridRadios2" value="{{ Constants::BANNER_SIDE }}" @if(old('type', $banner->type) == Constants::BANNER_SIDE) checked @endif>
+                                            <label class="form-check-label" for="gridRadios2">Side banner 500x280</label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="type" id="gridRadios2" value="{{ Constants::BANNER_CENTER }}" @if(old('type', $banner->type) == Constants::BANNER_CENTER) checked @endif>
+                                            <label class="form-check-label" for="gridRadios2">Center banner 700x70</label>
+                                        </div>
+                                    </div>
+                                </fieldset>
 
-                            <x-admin.form.input name="time" type="time" label="Time" :value="old('time', $banner->time)" :step="1">
-                                <x-admin.required-icon />
-                            </x-admin.form.input>
+                                <x-admin.form.input name="date" type="date" label="Published date" :value="old('date', $banner->date)">
+                                    <x-admin.required-icon />
+                                </x-admin.form.input>
 
-                            <x-admin.form.checkbox name="status" legend="active" :value="old('status', $banner->status)" :checked="old('status', $banner->status) == Constants::ACTIVE" />
+                                <x-admin.form.input name="time" type="time" label="Time" :value="old('time', $banner->time)" :step="1">
+                                    <x-admin.required-icon />
+                                </x-admin.form.input>
 
-                            <x-admin.form.button route="{{ route('admin.banners.index') }}">Edit</x-admin.form.button>
+                                <x-admin.form.checkbox name="status" legend="active" :value="old('status', $banner->status)" :checked="old('status', $banner->status) == Constants::ACTIVE" />
 
-                        </form><!-- End General Form Elements -->
+                                <x-admin.form.button route="{{ route('admin.banners.index') }}">Edit</x-admin.form.button>
+
+                            </form>
+                        </div>
                     </div>
-                </div><!-- End Left side columns -->
+                </div>
             </div>
         </section>
     </main><!-- End #main -->
