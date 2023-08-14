278bce804bacd9ed3f05af7503eb527796d54a8a
Nguyen-TH-Giang <124768986+Nguyen-TH-Giang@users.noreply.github.com>
2023-08-14 09:22:38 +0700

    Finished with newsletter

--- app/Models/NewsletterSubscriptions.php
+++ app/Models/NewsletterSubscriptions.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class NewsletterSubscriptions extends Model
 {
     use HasFactory;
+
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, function ($query, $search) {
+            $query->where('email', 'like', '%' . $search . '%');
+        });
+    }
 }
