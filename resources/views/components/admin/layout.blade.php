beb4f28f570ec60b456cd09a287ecd9f0f8454ca
Nguyen-TH-Giang <124768986+Nguyen-TH-Giang@users.noreply.github.com>
2023-08-16 15:03:33 +0700

    Activate back-to-top anchor, remove footer copyright, change the website name

--- resources/views/components/admin/layout.blade.php
+++ resources/views/components/admin/layout.blade.php
@@ -47,7 +47,7 @@
         <div class="d-flex align-items-center justify-content-between">
             <a href="/admin" class="logo d-flex align-items-center">
                 <img src="/backend/img/logo.png" alt="">
-                <span class="d-none d-lg-block">NiceAdmin</span>
+                <span class="d-none d-lg-block">Digital Magazine</span>
             </a>
             <i class="bi bi-list toggle-sidebar-btn"></i>
         </div><!-- End Logo -->
@@ -171,19 +171,10 @@
 
     <!-- ======= Footer ======= -->
     <footer id="footer" class="footer">
-        <div class="copyright">
-            &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
-        </div>
-        <div class="credits">
-            <!-- All the links in the footer should remain intact. -->
-            <!-- You can delete the links only if you purchased the pro version. -->
-            <!-- Licensing information: https://bootstrapmade.com/license/ -->
-            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-        </div>
+        <div class="copyright"><strong><span>Digital Magazine</span></strong></div>
     </footer><!-- End Footer -->
 
-    <a href="javascript:void(0)" class="back-to-top d-flex align-items-center justify-content-center"><i
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
             class="bi bi-arrow-up-short"></i></a>
 
     <!-- Vendor JS Files -->
